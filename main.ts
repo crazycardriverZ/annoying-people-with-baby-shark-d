194cfcede8ce900de39392263e9786ee97008385
crazycardriverZ <123813665+crazycardriverZ@users.noreply.github.com>
2023-02-25 12:42:13 +0000

    State variable

--- main.ts
+++ main.ts
@@ -1,72 +1,10 @@
 input.onButtonPressed(Button.A, function () {
     for (let index = 0; index < 9999999; index++) {
-        music.playTone(587, music.beat(BeatFraction.Whole))
-        music.playTone(659, music.beat(BeatFraction.Whole))
-        music.playTone(784, music.beat(BeatFraction.Half))
-        music.playTone(784, music.beat(BeatFraction.Half))
-        music.playTone(784, music.beat(BeatFraction.Half))
-        music.playTone(784, music.beat(BeatFraction.Quarter))
-        music.playTone(784, music.beat(BeatFraction.Quarter))
-        music.playTone(784, music.beat(BeatFraction.Quarter))
-        music.playTone(784, music.beat(BeatFraction.Quarter))
-        music.playTone(784, music.beat(BeatFraction.Half))
-        music.playTone(587, music.beat(BeatFraction.Half))
-        music.playTone(659, music.beat(BeatFraction.Half))
-        music.playTone(784, music.beat(BeatFraction.Half))
-        music.playTone(784, music.beat(BeatFraction.Half))
-        music.playTone(784, music.beat(BeatFraction.Half))
-        music.playTone(784, music.beat(BeatFraction.Quarter))
-        music.playTone(784, music.beat(BeatFraction.Quarter))
-        music.playTone(784, music.beat(BeatFraction.Quarter))
-        music.playTone(784, music.beat(BeatFraction.Quarter))
-        music.playTone(784, music.beat(BeatFraction.Half))
-        music.playTone(587, music.beat(BeatFraction.Half))
-        music.playTone(659, music.beat(BeatFraction.Half))
-        music.playTone(784, music.beat(BeatFraction.Half))
-        music.playTone(784, music.beat(BeatFraction.Half))
-        music.playTone(784, music.beat(BeatFraction.Half))
-        music.playTone(784, music.beat(BeatFraction.Quarter))
-        music.playTone(784, music.beat(BeatFraction.Quarter))
-        music.playTone(784, music.beat(BeatFraction.Quarter))
-        music.playTone(784, music.beat(BeatFraction.Quarter))
-        music.playTone(784, music.beat(BeatFraction.Half))
-        music.playTone(784, music.beat(BeatFraction.Half))
-        music.playTone(784, music.beat(BeatFraction.Half))
-        music.playTone(740, music.beat(BeatFraction.Whole))
-        basic.pause(500)
-        music.changeTempoBy(22)
+        State = 1
     }
 })
-input.onButtonPressed(Button.B, function () {
-    for (let index = 0; index < 99999999; index++) {
-        basic.showLeds(`
-            # # . . .
-            . . # # .
-            . . . . #
-            . . # # .
-            # # . . .
-            `)
-        basic.showLeds(`
-            . . . . .
-            # # # # .
-            . . . . #
-            # # # # .
-            . . . . .
-            `)
-        basic.showLeds(`
-            . . . . .
-            . . . . .
-            # # # # #
-            . . . . .
-            . . . . .
-            `)
-        basic.showLeds(`
-            . . . . .
-            # # # # .
-            . . . . #
-            # # # # .
-            . . . . .
-            `)
-    }
+input.onButtonPressed(Button.AB, function () {
+	
 })
+let State = 0
 music.setTempo(85)
