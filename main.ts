e2c49072c07da7bd5d58081ff6457d2e835ba247
crazycardriverZ <123813665+crazycardriverZ@users.noreply.github.com>
2023-02-25 12:26:37 +0000

    Snapping for annoying level 2

--- main.ts
+++ main.ts
@@ -1,4 +1,4 @@
-input.onButtonPressed(Button.AB, function () {
+input.onButtonPressed(Button.A, function () {
     for (let index = 0; index < 6; index++) {
         music.playTone(587, music.beat(BeatFraction.Whole))
         music.playTone(659, music.beat(BeatFraction.Whole))
@@ -34,5 +34,37 @@ input.onButtonPressed(Button.AB, function () {
         music.playTone(784, music.beat(BeatFraction.Half))
         music.playTone(740, music.beat(BeatFraction.Whole))
         basic.pause(500)
+    }
+})
+input.onButtonPressed(Button.B, function () {
+    for (let index = 0; index < 6; index++) {
+        basic.showLeds(`
+            # # . . .
+            . . # # .
+            . . . . #
+            . . # # .
+            # # . . .
+            `)
+        basic.showLeds(`
+            . . . . .
+            # # # # .
+            . . . . #
+            # # # # .
+            . . . . .
+            `)
+        basic.showLeds(`
+            . . . . .
+            . . . . .
+            # # # # #
+            . . . . .
+            . . . . .
+            `)
+        basic.showLeds(`
+            . . . . .
+            # # # # .
+            . . . . #
+            # # # # .
+            . . . . .
+            `)
     }
 })
